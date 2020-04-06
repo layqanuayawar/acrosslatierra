0969d3beae507be7a2e1239245304119e412dc95
owlsketch <satimidus@gmail.com>
2020-04-06 13:45:40 -0400

    Simulated VR Camera, Action Proxies, removed git plugin

---
+++ Assets/Scripts/TransformCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformCamera : MonoBehaviour
+{
+    public float standing;
+    public float crouching;
+
+    public void Crouch()
+    {
+        Vector3 originalPos = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(originalPos.x, crouching, originalPos.z);
+    }
+
+    public void Stand()
+    {
+        Vector3 originalPos = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(originalPos.x, standing, originalPos.z);
+    }
+}
